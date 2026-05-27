09bac5851b5f23134ce64cb2977e6134143958ac
israelalmore <israelalmore@gmail.com>
2026-05-27 22:55:10 +0200

    ...

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
   plugins: [react()],
+  base: "/portfolio-israel/", // ← nombre exacto de tu repo en GitHub
 });
